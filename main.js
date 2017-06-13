86bc199f6d2ffec3eeccdb99471f31aeca2640f9
bekihs <bekihs2003@gmail.com>
2017-06-13 11:58:30 +0300

    Create main.js

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-var compose = function (targetObj) {
+var composeObjects = function (targetObj) {
   var err = 'Error: all arguments passed in to compose must be objects';
 
   if (typeof targetObj !== 'object') {
@@ -19,3 +19,15 @@ var compose = function (targetObj) {
     return targetObj;
   }
 };
+
+function composeConstructors () {
+  var arrConstructors = arguments;
+
+  return function (params) {
+    for (var i = arrConstructors.length - 1; i > -1; i--) {
+      params = arrConstructors[i].call(this, params) || params;
+    }
+      
+     return params;
+  };
+}
